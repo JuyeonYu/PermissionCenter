499bfb24cf2d99f8469b5de48a021517ab25c45a
JuYeonYu <remake867@gmail.com>
2021-12-15 15:58:54 +0900

    add demo project

--- Example/PermissionCenter/ViewController.swift
+++ Example/PermissionCenter/ViewController.swift
@@ -7,18 +7,62 @@
 //
 
 import UIKit
+import PermissionCenter
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func onVideo(_ sender: Any) {
+        PermissionCenterString.notNow = "later"
+        PermissionCenter.video.action {
+            let alert = UIAlertController(title: "video", message: "has granted callback", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onAudio(_ sender: Any) {
+        PermissionCenter.audio.action {
+            let alert = UIAlertController(title: "video", message: "has granted callback", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }        }
     }
-
+    @IBAction func onGallery(_ sender: Any) {
+        PermissionCenter.gallery.action {
+            let alert = UIAlertController(title: "video", message: "has granted callback", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    @IBAction func onNotification(_ sender: Any) {
+        PermissionCenter.notification.action {
+            let alert = UIAlertController(title: "notification", message: "has granted callback", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    @IBAction func onContact(_ sender: Any) {
+        PermissionCenter.contact.action {
+            let alert = UIAlertController(title: "contact", message: "has granted callback", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    @IBAction func onLocation(_ sender: Any) {
+        PermissionCenter.location(type: .always).action {
+            let alert = UIAlertController(title: "location", message: "has granted callback", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
 }
 
